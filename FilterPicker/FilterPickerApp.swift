66c3742b3eaa0b0fd7435c85a5d638ef160a37ac
thisisdana49 <thisisdana49@gmail.com>
2025-05-11 19:03:22 +0900

    chore: 프로젝트 생성 및 Deployment Target 설정 #01

---
+++ FilterPicker/FilterPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilterPickerApp.swift
+//  FilterPicker
+//
+//  Created by 조다은 on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct FilterPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
