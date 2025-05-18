b8a389eff6cf5a9aa231f4dbeaba33ee60dfaeb2
thisisdana49 <thisisdana49@gmail.com>
2025-05-18 21:34:27 +0900

    chore: Font 파일 Resource에 추가 및 CustomFontName 종류 정의 #11

--- FilterPicker/Shared/DesignSystem/FontStyle.swift
+++ FilterPicker/Shared/DesignSystem/FontStyle.swift
@@ -9,24 +9,45 @@ import Foundation
 import SwiftUI
 
 enum FontStyle {
-    case title1
-    case body1
-    case body2
-    case body3
-    case caption1
-    case caption2
-    case caption3
-    
+    // Base: Pretendard
+    case title1         // 20, bold
+    case body1          // 16, medium
+    case body2          // 14, medium
+    case body3          // 13, medium
+    case caption1       // 12, regular
+    case caption2       // 10, regular
+    case caption3       // 8, regular
+
+    // Point: Mulgyeol
+    case mulgyeolTitle1 // 32, bold
+    case mulgyeolBody1  // 20, regular
+    case mulgyeolCaption1 // 14, regular
+
     var font: Font {
         switch self {
-        case .title1: return .system(size: 20, weight: .bold)
-        case .body1: return .system(size: 16, weight: .regular)
-        case .body2: return .system(size: 14, weight: .regular)
-        case .body3: return .system(size: 13, weight: .medium)
-        case .caption1: return .system(size: 12, weight: .regular)
-        case .caption2: return .system(size: 10, weight: .regular)
-        case .caption3: return .system(size: 8, weight: .regular)
+        // MARK: - Pretendard
+        case .title1:
+            return .custom(CustomFontName.pretendardBold, size: 20)
+        case .body1:
+            return .custom(CustomFontName.pretendardMedium, size: 16)
+        case .body2:
+            return .custom(CustomFontName.pretendardMedium, size: 14)
+        case .body3:
+            return .custom(CustomFontName.pretendardMedium, size: 13)
+        case .caption1:
+            return .custom(CustomFontName.pretendardRegular, size: 12)
+        case .caption2:
+            return .custom(CustomFontName.pretendardRegular, size: 10)
+        case .caption3:
+            return .custom(CustomFontName.pretendardRegular, size: 8)
+
+        // MARK: - Mulgyeol
+        case .mulgyeolTitle1:
+            return .custom(CustomFontName.mulgyeolBold, size: 32)
+        case .mulgyeolBody1:
+            return .custom(CustomFontName.mulgyeolRegular, size: 20)
+        case .mulgyeolCaption1:
+            return .custom(CustomFontName.mulgyeolRegular, size: 14)
         }
     }
 }
-
---
+++ FilterPicker/Shared/Extensions/View+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  View+Extension.swift
+//  FilterPicker
+//
+//  Created by 조다은 on 5/18/25.
+//
+
+import SwiftUI
+
+extension View {
+    func fontStyle(_ style: FontStyle) -> some View {
+        self.font(style.font)
+    }
+}
