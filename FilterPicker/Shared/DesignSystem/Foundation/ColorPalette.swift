fd504dd064b263293e185095b6763cabf1ab41c6
thisisdana49 <thisisdana49@gmail.com>
2025-05-20 15:00:14 +0900

    chore: Asset에 컬러 리스트 이름 수 #11

---
+++ FilterPicker/Shared/DesignSystem/Foundation/ColorPalette.swift
@@ -0,0 +1,26 @@
+//
+//  ColorPalette.swift
+//  FilterPicker
+//
+//  Created by 조다은 on 5/18/25.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    // MARK: - Turquoise
+    static let blackTurquoise = Color("fpBlackTurquoise")
+    static let deepTurquoise = Color("fpDeepTurquoise")
+    static let brightTurquoise = Color("fpBrightTurquoise")
+
+    // MARK: - Gray Scale
+    static let gray0 = Color("fpGray0")
+    static let gray15 = Color("fpGray15")
+    static let gray30 = Color("fpGray30")
+    static let gray45 = Color("fpGray45")
+    static let gray60 = Color("fpGray60")
+    static let gray75 = Color("fpGray75")
+    static let gray90 = Color("fpGray90")
+    static let gray100 = Color("fpGray100")
+}
