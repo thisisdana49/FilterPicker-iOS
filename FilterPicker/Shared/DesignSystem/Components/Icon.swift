92796d8455bf114d7274bbdcde6a2b866386131b
thisisdana49 <thisisdana49@gmail.com>
2025-05-20 19:32:06 +0900

    feat: Icon 컴포넌트화 #11

--- FilterPicker/Shared/DesignSystem/Components/Icon.swift
+++ FilterPicker/Shared/DesignSystem/Components/Icon.swift
@@ -5,4 +5,18 @@
 //  Created by 조다은 on 5/20/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct Icon: View {
+    let name: String
+    var size: CGFloat = 24
+    var color: Color = .fpGray90
+
+    var body: some View {
+        Image(name)
+            .renderingMode(.template)
+            .resizable()
+            .frame(width: size, height: size)
+            .foregroundColor(color)
+    }
+}
